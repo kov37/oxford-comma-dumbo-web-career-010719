7041fcb59d3e87dd8e6ce1ed69cec83733d1ee72
kovner chalumo <kchalumo@gmail.com>
2018-11-27 14:32:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     str = "#{array[0]} and #{array[1]}"
   else
     str = array.shift
-    array.each_with_index do |word|
+    array.each_with_index do |word, index|
       if index == array.length - 1 
         str << ", and #{array[index]}"
       elsif
